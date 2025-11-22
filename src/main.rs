011f54cb283b439a16cb783fd0fd93727cfe1e82
wogikaze <134709039+wogikaze@users.noreply.github.com>
2025-11-23 08:30:40 +0900

    Update to Dioxus 0.7 web scaffold (#3)
    * Update to Dioxus 0.7 web scaffold
    
    * fix to run

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use dioxus::prelude::*;
+
+fn main() {
+    launch(App);
+}
+
+#[component]
+fn App() -> Element {
+    rsx! {
+        main { class: "app",
+            h1 { "Dioxus Web" }
+            p { "Ready to build a reusable UI as a web library." }
+        }
+    }
+}
